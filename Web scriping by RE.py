66bfb430106f6224f922bcfa14ffc4e7d7904683
Sharan1425 <73060629+Sharan1425@users.noreply.github.com>
2020-11-12 22:03:34 +0530

    changed variable

--- Web scriping by RE.py
+++ Web scriping by RE.py
@@ -4,5 +4,5 @@ site = urllib.request.urlopen("https://www.maxfashion.in/in/en/contactus")
 data = site.read() # Here the data will be in binary format and we have to convert it into string format
 contacts = re.findall("[ +0-9]{3,4}[ ][0-9]{7,8}",str(data))
 print("The Apsrtc Contact numbers: ")
-for number in contacts:
-    print(number)
+for num in contacts:
+    print(num)
