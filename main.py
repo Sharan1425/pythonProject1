a297bf4cd779877227e09497426919d6dab4d4f2
Sharan1425 <73060629+Sharan1425@users.noreply.github.com>
2020-12-01 17:20:52 +0530

    changed variable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #fib
-n = int(input("Please enter the range of fib series: "))
+N = int(input("Please enter the range of fib series: "))
 a = 0
 b = 1
 print(a)
 print(b)
-for i in range(n):
+for i in range(N):
     c = a+b
     a,b = b,a+b
     print(c)
